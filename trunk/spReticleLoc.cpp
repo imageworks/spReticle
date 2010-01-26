117e3841dd4a46162a59e0cb3312e2b005e675e9
arminteractive <arminteractive@c655e972-7c62-11de-bb5b-0fd25fbaf517>
2010-01-26 22:29:00 +0000

    added Inc. to Sony Pictures Imageworks copyright line.

--- trunk/spReticleLoc.cpp
+++ trunk/spReticleLoc.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2009, Sony Pictures Imageworks
+// Copyright (c) 2009, Sony Pictures Imageworks Inc.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
